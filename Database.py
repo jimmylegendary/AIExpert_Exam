de98b3338ea8b030a78e4cd1d9438702f1188f90
Seungho Lee <jimmylee871124@gmail.com>
2022-08-30 17:09:27 +0900

    modify

--- Database.py
+++ Database.py
@@ -1,6 +1,17 @@
-import PyPDF2
 import fitz
+import json
+import os
 
+ans_keyword = [
+    '정답',
+    '(답)',
+    'Answer',
+    '답:',
+    '답변:',
+]
+
+ans_block = []
+i = 0
 class PDFData():
     def __init__(self, filename):
         self.text = ''
@@ -8,19 +19,51 @@ class PDFData():
         self.filename = filename
         
     def parse_pdf(self):
-        # pdf_file = open(self.filename, 'rb')
-        # read_pdf = PyPDF2.PdfFileReader(pdf_file)
-        # number_of_pages = read_pdf.getNumPages()
-        # page = read_pdf.getPage(0)
-        # page_content = page.extractText()
-        # print(page_content)
+        global i
         pdf_file = fitz.open(self.filename)
-        page = pdf_file[0]
-        pix = page.get_pixmap()
-        output = 'outfile.png'
-        pix.save(output)
+        for page in pdf_file:
+            pix = page.get_pixmap()
+            json_data = page.get_text('json')
+            data = json.loads(json_data)
+            for block in data['blocks']:
+                if block['type'] != 0: continue
+                for line in block['lines']:
+                    for span in line['spans']:
+                        text = span['text']
+                        is_ans = False
+                        for keyword in ans_keyword:
+                            if keyword in text:
+                                is_ans = True
+                        if is_ans:
+                            # print(text)
+                            ans_block.append((page,block))
+                            mat = fitz.Matrix(2, 2)  # zoom factor 2 in each direction
+                            bbox = block['bbox']
+                            clip = fitz.Rect(bbox)  # the area we want
+                            pix = page.get_pixmap(matrix=mat, clip=clip)
+                            pix.save(f'{i}.png','png')
+                            i += 1
+                        
+        # output = 'outfile.png'
+        # pix.save(output)
         
 if __name__ == '__main__':
     filename = 'test2.pdf'
-    pdfdata = PDFData(filename)
-    pdfdata.parse_pdf()
+    root_dir = '문제은행'
+    for (root, dirs, files) in os.walk(root_dir):
+        if len(files) > 0:
+            for file_name in files:
+                if 'pdf' in file_name:
+                    pdfdata = PDFData(f'{root}/{file_name}')
+                    pdfdata.parse_pdf()
+    
+    # for i, (page, block) in enumerate(ans_block):
+    #     mat = fitz.Matrix(2, 2)  # zoom factor 2 in each direction
+    #     bbox = block['bbox']
+    #     clip = fitz.Rect(bbox)  # the area we want
+    #     pix = page.get_pixmap(matrix=mat, clip=clip)
+    #     pix.save(f'{i}.png','png')
+        # for line in block['lines']:
+        #     for span in line['spans']:
+        #         text = span['text']
+        #         print(text.strip('\n'))
--- main.py
+++ main.py
@@ -1,31 +1,84 @@
+from fileinput import filename
 import tkinter as tk
-
-# tk = Tk()
-# label = 
-
-images = ['outfile.png', 'outfile1.png']
+import os
+import PIL
+from PIL import Image, ImageTk
 
 class ImageViewer():
     
-    def __init__(self, window):
-        self.canvas = tk.Canvas(window, width=600, height=400)
-        self.canvas.grid(row=0, column=0)
+    def __init__(self, window, img_files):
+        self.canvas_width = 1200
+        self.canvas_height = 300
+        self.pcanvas = tk.Canvas(window, width=self.canvas_width, height=self.canvas_height)
+        self.pcanvas.grid(row=0, column=0)
+        
+        self.acanvas = tk.Canvas(window, width=self.canvas_width, height=self.canvas_height)
+        self.acanvas.grid(row=1, column=0)
+        self.window = window
 
-        self.button = tk.Button(window, text="Load Image", command=self.on_button_clicked)
-        self.button.grid(row=1, column=0)
 
-        self.img = None
-        self.image_on_canvas = self.canvas.create_image(0, 0, anchor=tk.NW)
-        self.image_idx = 1
+        self.button = tk.Button(window, text="문제확인", command=self.show_problem)
+        self.button.grid(row=2, column=1)
+        self.button = tk.Button(window, text="정답확인", command=self.check_answer)
+        self.button.grid(row=2, column=2)
+        self.button = tk.Button(window, text="다음문제", command=self.next_problem)
+        self.button.grid(row=2, column=3)
+        
+        self.input_text = tk.Text(window, height=20, width=100)
+        self.input_text.grid(row=2,column=0)
 
-    def on_button_clicked(self):
-        self.img = tk.PhotoImage(file=images[self.image_idx])
-        self.canvas.itemconfig(self.image_on_canvas, image=self.img)
-        self.image_idx = 1 - self.image_idx
-        pass
+        self.img_files = img_files
+        self.p_idx = 0
+        self.sub_p_idx = 0
+
+    def show_problem(self):
+        image = Image.open(self.img_files[self.p_idx][self.sub_p_idx][0])
+        r_height = self.canvas_height
+        r_width = self.canvas_width
+        image = image.resize((r_height, r_width), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+        self.p_on_canvas = self.pcanvas.create_image(0, 0, anchor=tk.NW, image=photo)
+        # self.pcanvas.pack(side='top', expand=True,fill='both')
+        
+    def check_answer(self):       
+        image = Image.open(self.img_files[self.p_idx][self.sub_p_idx][1])
+        r_height = self.canvas_height
+        r_width = self.canvas_width
+        image = image.resize((r_height, r_width), PIL.Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(image)
+        self.a_on_canvas = self.acanvas.create_image(0, 0, anchor=tk.NW, image=photo)
+    
+    def next_problem(self):
+        sub_problem_size = len(self.img_files[self.p_idx])
+        self.sub_p_idx += 1
+        if self.sub_p_idx == sub_problem_size:
+            self.p_idx += 1
+            self.sub_p_idx = 0
 
 
 window = tk.Tk()
-window.title("Image Viewer Sample")
-ImageViewer(window)
+window.title("AIExpert Exam 2022")
+idx = 1
+root_dir = f'data'
+img_files = []
+for i in range(41):
+    for (root, dirs, files) in os.walk(root_dir+f'/{i}'):
+        pa_pair_files = []
+        for file_name in files:
+            if file_name.startswith('p'):
+                p_num = file_name.split('.')[0].split('_')
+                if len(p_num) > 1:
+                    p_num = p_num[1]
+                else:
+                    p_num =''
+                p_file = file_name
+                if p_num == '':
+                    ans_file = f'a{i}.png'
+                else:
+                    ans_file = f'a{i}_{p_num}.png'
+                p_file = f'{root}/{p_file}'
+                ans_file = f'{root}/{ans_file}'
+                pa_pair_files.append((p_file, ans_file))
+        img_files.append(pa_pair_files)    
+ImageViewer(window, img_files)
 window.mainloop()
